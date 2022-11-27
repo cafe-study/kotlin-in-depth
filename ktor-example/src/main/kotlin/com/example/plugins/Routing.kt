74d2d1416b1d40ef38187a0315c0958a8c30545e
silencer <silencerwon@gmail.com>
2022-11-28 00:19:05 +0900

    chapter 16.2.1 routing 예제

--- ktor-example/src/main/kotlin/com/example/plugins/Routing.kt
+++ ktor-example/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,16 +1,27 @@
 package com.example.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.routing.*
+import kotlinx.html.body
+import kotlinx.html.h1
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("hello/{userName?}") {
+            val userName = call.parameters["userName"] ?: "모르는 분"
+
+            call.respondHtml {
+                body {
+                    h1 { +"Hello, $userName" }
+                }
+            }
         }
     }
 }
